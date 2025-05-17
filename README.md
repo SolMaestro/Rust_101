2362566f446c84becde3a2c2c67342253aeb8595
SolMaestro <simplymufasah@gmail.com>
2025-05-17 06:04:13 +0100

    Updated basics.rs with a basic main function and also Edit README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rust_101
 This repo contains code samples and snippets i made from learning the Rust programming language.
+This repo will also include personal mini projects built using Rust.
---
+++ basics.rs
@@ -0,0 +1,6 @@
+//this file contains basic concepts and primitive code examples.
+
+fn main() {
+  println!("Hello Rust World!"); /*fn means function, main() is the function that starts a program in Rust, 
+                                  in this example it does not take an argument and retuns a unit type ()*/
+} //This prints "Hello Rust World!"
